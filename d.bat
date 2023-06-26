ce78af1163bf1a8f034dd8292597f5dcc012d3fd
De-Graft <36221402+grooveyman@users.noreply.github.com>
2023-06-26 12:17:12 +0000

    Add files via upload

---
+++ d.bat
@@ -0,0 +1,11 @@
+@echo off
+for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do vol %%a: 2>nul | find "APS AMANING" >nul && set myDrive=%%a:
+if "%myDrive%"=="" (echo Cannot find volume
+) else (
+echo Drive letter is %myDrive%)
+)
+
+attrib -h -r -s /s /d  %myDrive%\*.*
+del %myDrive%"\*.lnk" 2>logfile.txt
+del %myDrive%"\$RECYCLER.BIN" 2>logfile.txt
+pause
