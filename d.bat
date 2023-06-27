363d524a7ed213bee00491afdf802ac99d220450
De-Graft <36221402+grooveyman@users.noreply.github.com>
2023-06-27 09:25:28 +0000

    Update d.bat

--- d.bat
+++ d.bat
@@ -1,5 +1,6 @@
 @echo off
-for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do vol %%a: 2>nul | find "APS AMANING" >nul && set myDrive=%%a:
+set /p input = Enter drive name
+for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do vol %%a: 2>nul | find "%input%" >nul && set myDrive=%%a:
 if "%myDrive%"=="" (echo Cannot find volume
 ) else (
 echo Drive letter is %myDrive%)
